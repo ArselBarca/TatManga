f500b34acc5609a328ca980dd795049fabce7647
ArselBarca <yourbestf54@gmail.com>
2023-11-11 00:15:14 +0500

    почистил лишнее

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const app = express();
 // Настройка хранилища для загрузки файлов
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    // Папка с загруженными файлами будет зависеть от параметра в URL
     const manganame = req.params.manganame;
     cb(null, `manga/${manganame}`);
   },
@@ -25,13 +24,11 @@ app.post('/upload/:manganame', upload.array('files'), (req, res) => {
 // Доступ к загруженным файлам
 app.use('/manga/:manganame', express.static(path.join(__dirname, 'manga')));
 
-// Обработчик MIME-типов для CSS
 app.get('./css/style.css', (req, res) => {
   res.setHeader('Content-Type', 'text/css');
   res.sendFile(path.join(__dirname, 'style.css'));
 });
 
-// Обработчик MIME-типов для JavaScript
 app.get('./js/script.js', (req, res) => {
   res.setHeader('Content-Type', 'text/javascript');
   res.sendFile(path.join(__dirname, 'script.js'));
